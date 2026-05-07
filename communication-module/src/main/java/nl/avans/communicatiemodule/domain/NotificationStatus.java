697754e87814f5a98115291f294cfdf0b0bf9e08
AiboPeeters <aibo.peeters@gmail.com>
2026-05-07 13:15:31 +0200

    Revert "SaaS module prototype"
    This reverts commit 930107e63a459b1ad06ae9a54903a6a535a9f0c2.

--- communication-module/src/main/java/nl/avans/communicatiemodule/domain/NotificationStatus.java
+++
@@ -1,9 +0,0 @@
-package nl.avans.communicatiemodule.domain;
-
-public enum NotificationStatus {
-    PENDING,       // Appointment received, notifications not yet sent
-    PARTIAL,       // 24h sent, 1h not yet sent
-    COMPLETED,     // Both notifications sent
-    CANCELLED,     // Appointment was cancelled
-    FAILED         // All retries exhausted
-}
--- communication-module/src/main/java/nl/avans/communicatiemodule/domain/ProviderType.java
+++
@@ -1,8 +0,0 @@
-package nl.avans.communicatiemodule.domain;
-
-public enum ProviderType {
-    SWIFT_SEND,
-    LEGACY_LINK,
-    ASYNC_FLOW,
-    SECURE_POST
-}
